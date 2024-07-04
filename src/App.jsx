56f16acc15e696112e62ed8e29167267889dc459
Nexvaulus <moehammad.fikr@gmail.com>
2024-07-04 15:28:13 +0700

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import CreateProfile from "./pages/CreateProfile";
 import CreatePost from "./pages/CreatePost";
+import Notification from "./pages/Notification";
+import SearchPage from "./pages/SearchPage";
+
+import PageTester from "./pages/PageTester";
+
 import handleShowToast from "./functions/showToast";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -14,7 +19,7 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Login handleShowToast={handleShowToast} />} />
+				<Route path="/" element={<PageTester handleShowToast={handleShowToast} />} />
 				<Route
 					path="/login"
 					element={<Login handleShowToast={handleShowToast} />}
@@ -26,9 +31,12 @@ function App() {
 				<Route
 					path="/create"
 					element={<CreateProfile handleShowToast={handleShowToast} />}
+					
 				/>
 				<Route path="/post" element={<CreatePost />} />
 				<Route path="/home" element={<Home />} />
+				<Route path="/notification" element={<Notification />} />
+				<Route path="/search-page" element={<SearchPage/>} />
 				<Route path="*" element={<Error />} />
 			</Routes>
 
