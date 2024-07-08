91eb53b694c71e14b5887aa1052ca7abac95ce94
Nexvaulus <moehammad.fikr@gmail.com>
2024-07-08 17:57:34 +0700

    Finishing Notification Page

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,5 +1,6 @@
 import Friendlist from "../components/Friendlist";
 import Navbar from "../components/Navbar";
+import NotificationCard from "../components/NotificationCard";
 
 export default function Notification() {
   const activePage = "notification";
@@ -16,67 +17,42 @@ export default function Notification() {
 
               {/* Container section */}
               <div className="w-full flex flex-col mt-2 gap-2">
-                <div className="flex content-center box-border gap-2">
-                  <div className="w-8 flex justify-center content-center">
+                {/* Card Section */}
+                <div className="flex content-center box-border gap-2 md:gap-0">
+                  <div className="w-8 flex justify-center content-center md:w-12">
                     <img src="./icons/d-checkmark.svg" alt="like" className="cursor-pointer w-full" />
                   </div>
-                  {/* <div className="pt-1 text-xs w-3/4 sm:w-11/12"> */}
-                  <div className="pt-1 text-xs flex content-center w-3/4 ml-10 sm:w-5/6 sm:text-sm lg:w-full ">
-                    <p className="block">
+                  <div className="flex items-center w-3/4 ml-1 md:ml-3 sm:w-5/6 sm:text-sm lg:w-full ">
+                    <p className="inline-block align-bottom text-xs md:text-base">
                       <span className="font-semibold">Nickname</span> Lorem ipsum dolor, sit amet consectetur adipisicing el
                     </p>
                   </div>
-                  <div className="bg-d-text w-10 h-10 ml-auto"></div>
-                </div>
-
-                <div className="flex gap-2 p-1 content-center">
-                  <div className="w-8 flex justify-center content-center">
-                    <img src="./icons/d-checkmark.svg" alt="like" className="cursor-pointer w-full" />
-                  </div>
-                  <div className="py-1 flex text-sm">Lorem ipsum dolor sit.</div>
+                  <div className="bg-d-text w-10 h-10 ml-auto sm:w-12 sm:h-10 cursor-pointer"></div>
                 </div>
               </div>
 
-              {/* Container Section */}
-              <div className="w-full flex flex-col mt-2 gap-3">
+              <div className="w-full flex flex-col mt-2 gap-2">
                 <div className="flex gap-2 p-1 content-center">
                   <div className="py-1 flex text-md font-bold">Hari ini</div>
                 </div>
 
-                <div className="flex gap-2 p-1 content-center">
-                  <div className="w-8 h-8 flex justify-center content-center">
-                    <div className="w-full h-full rounded-full bg-d-bgc" id="image"></div>
-                  </div>
-                  <div className="py-1 flex text-sm">Lorem ipsum dolor sit.</div>
-                </div>
-
-                <div className="flex gap-2 p-1 content-center">
-                  <div className="w-8 h-8 flex justify-center content-center">
-                    <div className="w-full h-full rounded-full bg-d-bgc" id="image"></div>
-                  </div>
-                  <div className="py-1 flex text-sm">Lorem ipsum dolor sit.</div>
-                </div>
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
               </div>
 
-              <div className="w-full flex flex-col mt-2 gap-3">
+              <div className="w-full flex flex-col mt-2 gap-2">
                 <div className="flex gap-2 p-1 content-center">
-                  <div className="py-1 flex text-md font-bold">Hari ini</div>
+                  <div className="py-1 flex text-md font-bold">Kemarin</div>
                 </div>
 
-                <div className="flex gap-2 p-1 content-center">
-                  <div className="w-8 h-8 flex justify-center content-center">
-                    <div className="w-full h-full rounded-full bg-d-bgc" id="image"></div>
-                  </div>
-                  <div className="py-1 flex text-sm">Lorem ipsum dolor sit.</div>
-                </div>
-
-                <div className="flex gap-2 p-1 content-center">
-                  <div className="w-8 h-8 flex justify-center content-center">
-                    <div className="w-full h-full rounded-full bg-d-bgc" id="image"></div>
-                  </div>
-                  <div className="py-1 flex text-sm">Lorem ipsum dolor sit.</div>
-                </div>
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
+                <NotificationCard />
               </div>
+
             </div>
           </div>
           {/* End Posts */}
