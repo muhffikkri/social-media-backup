9d3797af20e7512562083a37e817ea8ab9fd8c96
RandDevs <randdevs54@gmail.com>
2024-09-06 23:28:02 +0700

    Add dropdown in settings menu

--- src/components/Private/Layout.jsx
+++ src/components/Private/Layout.jsx
@@ -3,17 +3,17 @@ import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 export default function Layout() {
-	const [isDarkMode, setDarkMode] = useState(true);
-	const [activePage, setActivePage] = useState("home-page");
-	return (
-		<>
-			<Navbar
-				isDarkMode={isDarkMode}
-				setDarkMode={setDarkMode}
-				activePage={activePage}
-				setActivePage={setActivePage}
-			/>
-			<Outlet context={{ isDarkMode, setActivePage }} />
-		</>
-	);
+  const [isDarkMode, setDarkMode] = useState(true);
+  const [activePage, setActivePage] = useState("home-page");
+  return (
+    <>
+      <Navbar
+        isDarkMode={isDarkMode}
+        setDarkMode={setDarkMode}
+        activePage={activePage}
+        setActivePage={setActivePage}
+      />
+      <Outlet context={{ isDarkMode, setActivePage }} />
+    </>
+  );
 }
